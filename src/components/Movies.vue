0431d180212eed6290ff5bb06a5ad41e216fab73
Ross Johnson <ross.johnson@itech.media>
2017-11-23 10:17:27 +0000

    add sorting buttons

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="title">
     <h1>{{type}} Movies</h1>
-    <button v-on:click="shuffle">Shuffle</button>
+    <button @click="shuffle">Shuffle</button>
+    Alphabetical:
+    <button @click="sort('title')">ASC</button>
+    <button @click="sort('title', 'desc')">DESC</button>
+    Rating:
+    <button @click="sort('vote_average')">ASC</button>
+    <button @click="sort('vote_average', 'desc')">DESC</button>
     <transition-group name="flip-list" tag="ul">
       <li v-for="movie in movies" v-bind:key="movie.title">
-<!--        {{ movie.title }}-->
-        <img :src="'https://image.tmdb.org/t/p/w154/' + movie.poster_path" alt="" />
+        {{ movie.title }}
+        <!--<img :src="'https://image.tmdb.org/t/p/w154/' + movie.poster_path" alt="" />-->
       </li>
     </transition-group>
   </div>
@@ -14,6 +20,7 @@
 <script>
 import axios from 'axios'
 import shuffle from 'lodash/shuffle'
+import sortBy from 'lodash/sortBy'
 export default {
   name: 'Movies',
   props: ['type'],
@@ -28,18 +35,29 @@ export default {
   methods: {
     shuffle: function () {
       this.movies = shuffle(this.movies)
+    },
+
+    sort: function (sort, direction = 'asc') {
+      // always have a secondary sort option in case movies have the same rating
+      sort = [sort, 'title']
+      this.movies = sortBy(this.movies, sort)
+      if (direction === 'desc') this.movies.reverse()
+    },
+
+    fetch_data () {
+      axios.get('https://api.themoviedb.org/3/movie/' + this.type + '?api_key=07a435bd643821239b3f9beb1a1027cb')
+        .then(response => {
+          console.log(response.data.results)
+          this.movies = response.data.results
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
     }
   },
 
   created () {
-    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=07a435bd643821239b3f9beb1a1027cb')
-    .then(response => {
-      console.log(response.data.results)
-      this.movies = response.data.results
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
+    this.fetch_data()
   }
 }
 </script>
